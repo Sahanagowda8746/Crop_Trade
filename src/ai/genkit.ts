d54fedfb79412c8fb42554bfc3f36c4c72d61178
Sahana Gowda <sahanagowdasahana121@gmail.com>
2025-11-18 15:04:10 +0000

    see now its coming like this

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,5 +4,5 @@ import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: 'googleai/gemini-pro-vision',
+  model: 'googleai/gemini-pro',
 });
